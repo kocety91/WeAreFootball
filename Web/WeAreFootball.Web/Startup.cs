ca3713664a53ede1b7b18ed082c3d2c498898ecc
kocety91 <kocety91@gmail.com>
2021-08-06 15:22:53 +0300

    Update Startup

--- Web/WeAreFootball.Web/Startup.cs
+++ Web/WeAreFootball.Web/Startup.cs
@@ -1,18 +1,10 @@
 ﻿namespace WeAreFootball.Web
 {
+    using System;
     using System.Reflection;
 
-    using WeAreFootball.Data;
-    using WeAreFootball.Data.Common;
-    using WeAreFootball.Data.Common.Repositories;
-    using WeAreFootball.Data.Models;
-    using WeAreFootball.Data.Repositories;
-    using WeAreFootball.Data.Seeding;
-    using WeAreFootball.Services.Data;
-    using WeAreFootball.Services.Mapping;
-    using WeAreFootball.Services.Messaging;
-    using WeAreFootball.Web.ViewModels;
-
+    using Hangfire;
+    using Hangfire.SqlServer;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Http;
@@ -21,6 +13,17 @@
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
+    using WeAreFootball.Data;
+    using WeAreFootball.Data.Common;
+    using WeAreFootball.Data.Common.Repositories;
+    using WeAreFootball.Data.Models;
+    using WeAreFootball.Data.Repositories;
+    using WeAreFootball.Data.Seeding;
+    using WeAreFootball.Services.Data;
+    using WeAreFootball.Services.Data.Contracts;
+    using WeAreFootball.Services.Mapping;
+    using WeAreFootball.Services.Messaging;
+    using WeAreFootball.Web.ViewModels;
 
     public class Startup
     {
@@ -42,18 +45,38 @@
 
             services.Configure<CookiePolicyOptions>(
                 options =>
-                    {
-                        options.CheckConsentNeeded = context => true;
-                        options.MinimumSameSitePolicy = SameSiteMode.None;
-                    });
+                {
+                    options.CheckConsentNeeded = context => true;
+                    options.MinimumSameSitePolicy = SameSiteMode.None;
+                });
+
+            // Social Network Authentication
+            services.AddAuthentication()
+                .AddFacebook(facebookOptions =>
+                {
+                    facebookOptions.AppId = this.configuration["FacebookAuthSettings:AppId"];
+                    facebookOptions.AppSecret = this.configuration["FacebookAuthSettings:AppSecret"];
+                })
+               .AddGoogle(googleOptions =>
+               {
+                   googleOptions.ClientId = this.configuration["GoogleAuthSettings:ClientId"];
+                   googleOptions.ClientSecret = this.configuration["GoogleAuthSettings:ClientSecret"];
+               });
 
             services.AddControllersWithViews(
                 options =>
-                    {
-                        options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
-                    }).AddRazorRuntimeCompilation();
+                {
+                    options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
+                }).AddRazorRuntimeCompilation();
             services.AddRazorPages();
             services.AddDatabaseDeveloperPageExceptionFilter();
+
+            services.AddSingleton(this.configuration);
+
+            services.AddAntiforgery(options =>
+            {
+                options.HeaderName = "X-CSRF-TOKEN";
+            });
 
             services.AddSingleton(this.configuration);
 
@@ -65,10 +88,33 @@
             // Application services
             services.AddTransient<IEmailSender, NullMessageSender>();
             services.AddTransient<ISettingsService, SettingsService>();
+            services.AddTransient<IUsersService, UsersService>();
+            services.AddTransient<INewsService, NewsService>();
+            services.AddTransient<ILeaguesService, LeaguesService>();
+            services.AddTransient<ITeamsService, TeamsService>();
+            services.AddTransient<IVotesService, VotesService>();
+            services.AddTransient<ICommentsService, CommentsService>();
+            services.AddTransient<IEventsService, EventsService>();
+            services.AddTransient<ITagsService, TagsService>();
+            services.AddTransient<IContactsService, ContactsService>();
+
+            services.AddHangfire(
+              config => config.SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                  .UseSimpleAssemblyNameTypeSerializer().UseRecommendedSerializerSettings().UseSqlServerStorage(
+                      this.configuration.GetConnectionString("DefaultConnection"),
+                      new SqlServerStorageOptions
+                      {
+                          CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+                          SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+                          QueuePollInterval = TimeSpan.Zero,
+                          UseRecommendedIsolationLevel = true,
+                          UsePageLocksOnDequeue = true,
+                          DisableGlobalLocks = true,
+                      }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IRecurringJobManager recurringJobManager)
         {
             AutoMapperConfig.RegisterMappings(typeof(ErrorViewModel).GetTypeInfo().Assembly);
 
@@ -91,6 +137,7 @@
                 app.UseHsts();
             }
 
+            app.UseStatusCodePagesWithRedirects("/Home/HttpError?statusCode={0}");
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
@@ -100,13 +147,25 @@
             app.UseAuthentication();
             app.UseAuthorization();
 
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+
+            this.SeedHangfireJob(recurringJobManager);
+
             app.UseEndpoints(
                 endpoints =>
-                    {
-                        endpoints.MapControllerRoute("areaRoute", "{area:exists}/{controller=Home}/{action=Index}/{id?}");
-                        endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
-                        endpoints.MapRazorPages();
-                    });
+                {
+                    endpoints.MapControllerRoute("areaRoute", "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                    endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+                    endpoints.MapRazorPages();
+
+                    endpoints.MapHangfireDashboard();
+                });
+        }
+
+        private void SeedHangfireJob(IRecurringJobManager recurringJobManager)
+        {
+            recurringJobManager.AddOrUpdate<NewsService>("Scrape", x => x.Scrape(), Cron.Daily(2));
         }
     }
 }
