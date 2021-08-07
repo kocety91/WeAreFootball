b7a1d56034fdef174272afe9fe7a13807d43f888
kocety91 <kocety91@gmail.com>
2021-08-07 10:55:38 +0300

    Add Home And Contacts Controller

---
+++ Web/WeAreFootball.Web/Controllers/ContactsController.cs
@@ -0,0 +1,41 @@
+﻿namespace WeAreFootball.Web.Controllers
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+    using WeAreFootball.Services.Data.Contracts;
+    using WeAreFootball.Web.ViewModels.ContacktForm;
+
+    public class ContactsController : BaseController
+    {
+        private readonly IContactsService contactsService;
+
+        public ContactsController(IContactsService contactsService)
+        {
+            this.contactsService = contactsService;
+        }
+
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(ContactFormInputViewModel contactFormViewModel)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(contactFormViewModel);
+            }
+
+            await this.contactsService.Send(contactFormViewModel);
+
+            return this.RedirectToAction("ThankYou");
+        }
+
+        public IActionResult ThankYou()
+        {
+            return this.View();
+        }
+    }
+}
--- Web/WeAreFootball.Web/Controllers/HomeController.cs
+++ Web/WeAreFootball.Web/Controllers/HomeController.cs
@@ -2,15 +2,50 @@
 {
     using System.Diagnostics;
 
-    using WeAreFootball.Web.ViewModels;
-
     using Microsoft.AspNetCore.Mvc;
+    using WeAreFootball.Services.Data.Contracts;
+    using WeAreFootball.Web.ViewModels;
+    using WeAreFootball.Web.ViewModels.Events;
+    using WeAreFootball.Web.ViewModels.Home;
+    using WeAreFootball.Web.ViewModels.Leagues;
+    using WeAreFootball.Web.ViewModels.Newses;
 
     public class HomeController : BaseController
     {
-        public IActionResult Index()
+        private readonly INewsService newsService;
+        private readonly IEventsService eventsService;
+        private readonly ILeaguesService leaguesService;
+
+        public HomeController(
+            INewsService newsService,
+            IEventsService eventsService,
+            ILeaguesService leaguesService)
         {
-            return this.View();
+            this.newsService = newsService;
+            this.eventsService = eventsService;
+            this.leaguesService = leaguesService;
+        }
+
+        public IActionResult Index(int id = 1)
+        {
+            if (id <= 0)
+            {
+                return this.NotFound();
+            }
+
+            var itemsPerPage = 6;
+            var viewModel = new IndexViewModel
+            {
+                ItemsPerPage = itemsPerPage,
+                PageNumber = id,
+                Count = this.newsService.GetCount(),
+                News =
+                    this.newsService.GetAll<NewsViewModel>(id, itemsPerPage),
+                MostCommentNews = this.newsService.GetMostComment<NewsViewModel>(),
+                EventsToday = this.eventsService.TodayEvents<EventViewModel>(),
+                LeaguesCountry = this.leaguesService.GetAll<LeagueViewModel>(),
+            };
+            return this.View(viewModel);
         }
 
         public IActionResult Privacy()
@@ -19,10 +54,27 @@
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Error(HttpErrorViewModel errorViewModel)
         {
+            if (errorViewModel.StatusCode == 404)
+            {
+                return this.View(
+                "Error", new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
+            }
+
             return this.View(
                 new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult HttpError(HttpErrorViewModel errorViewModel)
+        {
+            if (errorViewModel.StatusCode == 404)
+            {
+                return this.View(errorViewModel);
+            }
+
+            return this.View(
+                "Error", new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
         }
     }
 }
