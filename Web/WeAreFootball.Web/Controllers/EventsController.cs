fcdf90c65c0cfa05e47ebc82fe6b66545699b5b4
kocety91 <kocety91@gmail.com>
2021-08-07 10:59:18 +0300

    Add Events And Leagues Controllers

---
+++ Web/WeAreFootball.Web/Controllers/EventsController.cs
@@ -0,0 +1,59 @@
+﻿namespace WeAreFootball.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using WeAreFootball.Services.Data.Contracts;
+    using WeAreFootball.Web.ViewModels.Events;
+
+    public class EventsController : BaseController
+    {
+        private readonly IEventsService eventsService;
+
+        public EventsController(
+            IEventsService eventsService)
+        {
+            this.eventsService = eventsService;
+        }
+
+        public IActionResult All(int id = 1)
+        {
+            if (id <= 0)
+            {
+                return this.NotFound();
+            }
+
+            var itemsPerPage = 6;
+
+            var viewModel = new AllEventsViewModel()
+            {
+                ItemsPerPage = itemsPerPage,
+                PageNumber = id,
+                Count = this.eventsService.GetCount(),
+                Events = this.eventsService.GetAll<EventViewModel>(id, itemsPerPage),
+            };
+
+            return this.View(viewModel);
+        }
+
+        public IActionResult ById(int id)
+        {
+            var currentEvent = this.eventsService.GetById<EventViewModel>(id);
+            return this.View(currentEvent);
+        }
+
+        public IActionResult ByLeagueId(int leagueId)
+        {
+            var itemsPerPage = 6;
+            var pageId = 1;
+
+            var viewModel = new AllEventsViewModel()
+            {
+                ItemsPerPage = itemsPerPage,
+                PageNumber = pageId,
+                Count = this.eventsService.GetCount(),
+                Events = this.eventsService.GetByLeagueId<EventViewModel>(leagueId, pageId, itemsPerPage),
+            };
+
+            return this.View(viewModel);
+        }
+    }
+}
---
+++ Web/WeAreFootball.Web/Controllers/LeaguesController.cs
@@ -0,0 +1,33 @@
+﻿namespace WeAreFootball.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using WeAreFootball.Services.Data.Contracts;
+    using WeAreFootball.Web.ViewModels.Leagues;
+
+    public class LeaguesController : BaseController
+    {
+        private readonly ILeaguesService leaguesService;
+
+        public LeaguesController(
+            ILeaguesService leaguesService)
+        {
+            this.leaguesService = leaguesService;
+        }
+
+        public IActionResult All()
+        {
+            var viewModel = new AllLeaguesViewModel()
+            {
+                Leagues = this.leaguesService.All<LeagueViewModel>(),
+            };
+
+            return this.View(viewModel);
+        }
+
+        public IActionResult ById(int id)
+        {
+            var league = this.leaguesService.GetById<LeagueViewModel>(id);
+            return this.View(league);
+        }
+    }
+}
