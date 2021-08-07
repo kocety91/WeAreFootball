53021a53e7a4291e53d538664ec1183d8170ae69
kocety91 <kocety91@gmail.com>
2021-08-07 11:45:32 +0300

    Update Identity

---
+++ Web/WeAreFootball.Web/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -0,0 +1,83 @@
+﻿namespace AspNetCoreTemplate.Web.Areas.Identity.Pages.Account.Manage
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using WeAreFootball.Data.Models;
+    using WeAreFootball.Services.Data.Contracts;
+    using WeAreFootball.Web.ViewModels.Administration;
+
+    public partial class IndexModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly IUsersService usersService;
+        private readonly IWebHostEnvironment environment;
+
+        public IndexModel(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            IUsersService usersService,
+            IWebHostEnvironment environment)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.usersService = usersService;
+            this.environment = environment;
+        }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await this.userManager.GetUserAsync(this.User);
+            if (user == null)
+            {
+                return this.NotFound($"Unable to load user with ID '{this.userManager.GetUserId(this.User)}'.");
+            }
+
+            await this.LoadAsync(user);
+            return this.Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var user = await this.userManager.GetUserAsync(this.User);
+            if (user == null)
+            {
+                return this.NotFound($"Unable to load user with ID '{this.userManager.GetUserId(this.User)}'.");
+            }
+
+            if (!this.ModelState.IsValid)
+            {
+                this.StatusMessage = "Image error";
+                await this.LoadAsync(user);
+                return this.Page();
+            }
+
+            await this.usersService
+                .UploadImageToUser(this.Input, user.Id, $"{this.environment.WebRootPath}/images");
+
+            await this.signInManager.RefreshSignInAsync(user);
+            this.StatusMessage = "Your profile has been updated";
+            return this.RedirectToPage();
+        }
+
+        private async Task LoadAsync(ApplicationUser user)
+        {
+            var userName = await this.userManager.GetUserNameAsync(user);
+
+            this.Input = new InputModel
+            {
+                Username = userName,
+            };
+        }
+    }
+}
