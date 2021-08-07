2dc3f20cece68f2793b375d9c7492b38aac067eb
kocety91 <kocety91@gmail.com>
2021-08-07 10:57:47 +0300

    Add Comments And Votes Controllers

---
+++ Web/WeAreFootball.Web/Controllers/CommentsController.cs
@@ -0,0 +1,47 @@
+﻿namespace WeAreFootball.Web.Controllers
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using WeAreFootball.Data.Models;
+    using WeAreFootball.Services.Data.Contracts;
+    using WeAreFootball.Web.ViewModels.Comments;
+
+    public class CommentsController : BaseController
+    {
+        private readonly ICommentsService commentsService;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public CommentsController(
+            ICommentsService commentsService,
+            UserManager<ApplicationUser> userManager)
+        {
+            this.commentsService = commentsService;
+            this.userManager = userManager;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Create(CreeateCommentViewModel input)
+        {
+            var parentId =
+                 input.ParentId == 0 ?
+                     (int?)null :
+                     input.ParentId;
+
+            if (parentId.HasValue)
+            {
+                if (!this.commentsService.IsInPostId(parentId.Value, input.NewsId))
+                {
+                    return this.BadRequest();
+                }
+            }
+
+            var userId = this.userManager.GetUserId(this.User);
+            await this.commentsService.Create(input.NewsId, userId, input.Content, parentId);
+            return this.RedirectToAction("ById", "News", new { id = input.NewsId });
+        }
+    }
+}
---
+++ Web/WeAreFootball.Web/Controllers/VotesController.cs
@@ -0,0 +1,32 @@
+﻿namespace WeAreFootball.Web.Controllers
+{
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using WeAreFootball.Services.Data.Contracts;
+    using WeAreFootball.Web.ViewModels.Votes;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VotesController : BaseController
+    {
+        private readonly IVotesService votesService;
+
+        public VotesController(IVotesService votesService)
+        {
+            this.votesService = votesService;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<ActionResult<PostVoteResponseViewModel>> Post(PostVoteViewModel input)
+        {
+            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            await this.votesService.SetVoteAsync(input.EventId, userId, input.Value);
+            var averageVotes = this.votesService.GetAverageVotes(input.EventId);
+            return new PostVoteResponseViewModel { AverageVote = averageVotes };
+        }
+    }
+}
