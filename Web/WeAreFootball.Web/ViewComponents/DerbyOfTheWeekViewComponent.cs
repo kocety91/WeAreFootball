8628981fea0888f673cbb3452d08fb0e951c4e59
kocety91 <kocety91@gmail.com>
2021-08-07 11:23:14 +0300

    Add View Components

---
+++ Web/WeAreFootball.Web/ViewComponents/DerbyOfTheWeekViewComponent.cs
@@ -0,0 +1,23 @@
+﻿namespace WeAreFootball.Web.ViewComponents
+{
+    using Microsoft.AspNetCore.Mvc;
+    using WeAreFootball.Services.Data.Contracts;
+    using WeAreFootball.Web.ViewModels.Events;
+
+    public class DerbyOfTheWeekViewComponent : ViewComponent
+    {
+        private readonly IEventsService eventsService;
+
+        public DerbyOfTheWeekViewComponent(IEventsService eventsService)
+        {
+            this.eventsService = eventsService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var derby = this.eventsService.GetLatestDerbyOfTheWeek<DerbyOfTheWeekViewModel>();
+
+            return this.View(derby);
+        }
+    }
+}
---
+++ Web/WeAreFootball.Web/ViewComponents/NavBarComponent.cs
@@ -0,0 +1,33 @@
+﻿namespace WeAreFootball.Web.ViewComponents
+{
+    using Microsoft.AspNetCore.Mvc;
+    using WeAreFootball.Services.Data.Contracts;
+    using WeAreFootball.Web.ViewModels.Events;
+    using WeAreFootball.Web.ViewModels.Leagues;
+    using WeAreFootball.Web.ViewModels.NavBars;
+
+    public class NavBarComponent : ViewComponent
+    {
+        private readonly ILeaguesService leaguesService;
+        private readonly IEventsService eventsService;
+
+        public NavBarComponent(
+            ILeaguesService leaguesService,
+            IEventsService eventsService)
+        {
+            this.leaguesService = leaguesService;
+            this.eventsService = eventsService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var myNavBar = new NavBarViewModel()
+            {
+                Leagues = this.leaguesService.GetAll<LeagueViewModel>(),
+                LastFourEvents = this.eventsService.GetLastFour<EventViewModel>(),
+            };
+
+            return this.View(myNavBar);
+        }
+    }
+}
