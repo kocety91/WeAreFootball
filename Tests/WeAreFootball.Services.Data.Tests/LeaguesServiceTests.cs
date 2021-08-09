d95b52f6834838e52864d86be62e9aa3736116bb
kocety91 <kocety91@gmail.com>
2021-08-09 15:47:47 +0300

    Create LeaguesServiceTests.cs

---
+++ Tests/WeAreFootball.Services.Data.Tests/LeaguesServiceTests.cs
@@ -0,0 +1,21 @@
+﻿namespace WeAreFootball.Services.Data.Tests
+{
+    using System.Reflection;
+
+    using WeAreFootball.Services.Mapping;
+    using WeAreFootball.Web.ViewModels;
+
+    public class LeaguesServiceTests
+    {
+        public LeaguesServiceTests()
+        {
+            this.InitializeMapper();
+        }
+
+
+
+
+        private void InitializeMapper() => AutoMapperConfig.
+          RegisterMappings(typeof(ErrorViewModel).GetTypeInfo().Assembly);
+    }
+}
